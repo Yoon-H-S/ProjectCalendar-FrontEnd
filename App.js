ba6cd18c2fdc1937584f08caa08353fa66aeecd0
YHS <dlddu7064@naver.com>
2023-11-02 23:13:31 +0900

    패키지 설치(웹팩, 캘린더(직접 수정), 상태바높이)

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { getStatusBarHeight } from "react-native-status-bar-height"; 
+
+import CustomCalendar from './src/component/CustomCalendar';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Project Calendar</Text>
-            <StatusBar style="auto" />
+            <CustomCalendar />
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: getStatusBarHeight(),
+        marginHorizontal: 16,
     },
 });
